27d59671b64f0e53c3d70abb6580633bd27096d4
Rohan Kulkarni <rohank2502@gmail.com>
2023-04-07 18:54:45 +0530

    about htl

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,10 @@ import {
 function App() {
   const [data, setData] = useState([]);
 
+
   useEffect(() => {
     const fetchData = async () => {
+      console.log("Geting");
       try {
         const response = await fetch(
           "https://tweet-api-mentions-production.up.railway.app/api/v1/tweets"
@@ -23,35 +25,37 @@ function App() {
         const newData = await response.json();
         let latestData = newData.data;
         latestData.length = 20;
+        // if (latestData[0].id !== data[0].id) {
+        // console.log('in');
         setData(latestData);
+        // } else {
+        // return;
+        // }
       } catch (error) {
         console.error(error);
       }
+      setInterval(fetchData, 30000);
     };
-
     fetchData();
   }, []);
-  function generateRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-   
+
   return (
     <div className="bg-blue-950">
-    <h1 className="text-white text-center pt-8 text-8xl font-bold">HACK THE LEAGUE 2.0</h1>
-    <div className="p-12 mt-2 grid grid-cols-4 gap-4 justify-evenly ">
-
-      {data.map((item) => {
-        console.log(Math.floor(Math.random() * 6));
-        return (
-
-
-          <div className={`mt-${generateRandomInteger(1,4)} mb-${generateRandomInteger(1,4)}  overflow-scroll h-128 w-full place-self-center rotate-${generateRandomInteger(1,4)}`}>
-            <TwitterTweetEmbed key={item.id} tweetId={item.id} />
- 
-          </div>
-        );
-      })}
-    </div>
+      <h1 className="text-white text-center pt-8 text-8xl font-bold">
+        HACK THE LEAGUE 2.0
+      </h1>
+      <p className="text-center text-white text-2xl pt-8 font-mono">tweet your experience</p>
+      <div className="p-12 mt-2 grid grid-cols-4 gap-4 justify-evenly ">
+        {data.map((item) => {
+          return (
+            <div
+              className={`mt-4 mb-4 overflow-clip h-128 w-full place-self-center  `}
+            >
+              <TwitterTweetEmbed key={item.created_at} tweetId={item.id} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
